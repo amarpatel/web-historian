83dbde5c1779e62c4b009fb842db027014ff766c
Amar Patel <theamarpatel@gmail.com>
2014-05-11 21:32:45 -0700

    added website scraping functionality

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,29 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
+var archive = require('../helpers/archive-helpers');
+var httpRequest = require('http-request');
+var _ = require('underscore');
+var path = require('path');
+
+var fetch = function () {
+  archive.readListOfUrls(function(sitesArray){
+    _.each(sitesArray, function (val, i, col) {
+      archive.isURLArchived(val, function (exists) {
+        if (!exists) {
+          httpRequest.get({
+            url: val, 
+            progress: function (current, total) {
+              console.log('downloaded %d bytes', current, total);
+            },
+          },
+            path.join(archive.paths.archivedSites, val), function (err, res) {
+              if (err) console.log(err);
+            }
+          );
+        }
+      });
+    });
+  });
+};
+
+fetch();
